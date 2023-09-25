08c7c5823497cbd1b8c5ab563591a2eec471d637
maxeq <maxeq1@gmail.com>
2023-09-25 20:19:07 +0200

    x

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-export default function Home() {
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+});
+
+
+export default async function Home() {
+  const todos = await prisma.dyna.findMany();
+  const title = todos?.[0]?.title;
+  const description = todos?.[0]?.description;
+  const keyword = todos?.[0]?.keyword;
   return (
     <section className='py-24'>
       <div className='container'>
         <h1 className='text-3xl font-bold'>
-          NextJs 13 Starter Kit (TypeScript)
+          {title}
+          {description}
+          {keyword}
         </h1>
       </div>
     </section>
